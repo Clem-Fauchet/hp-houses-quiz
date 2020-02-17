f11c01c579707aeec1e7c61fd41f1b3e1a6ecab7
Clem <clemence.fauchet23@gmail.com>
2020-02-17 15:13:27 +0100

    First deployemet github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 
 import './styles/App.scss'
 import quizQuestions from './api/quizQuestions'
